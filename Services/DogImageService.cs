dc088f5fe6b599920ec316683a0d6e5c11a92954
MeabhG97 <91346624+Mayv-dev@users.noreply.github.com>
2024-10-12 18:04:36 +0100

    Getting Image Url

---
+++ Services/DogImageService.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text.Json;
+using Dogs.Types;
+using RestSharp;
+
+namespace Dogs.Services
+{
+    public class DogImageService {
+        static readonly string DOG_IMAGE_URL = "https://random.dog/woof.json";
+
+        public string GetImage() {
+            var client = new RestClient(DOG_IMAGE_URL);
+            var req = new RestRequest();
+
+            var res = client.Execute(req);
+            
+            var content = JsonSerializer.Deserialize<DogImageResponse>(res.Content);
+            return content.ImageUrl;
+        }
+    }
+}
