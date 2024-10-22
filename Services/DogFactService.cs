9a332054e6aa959607efc1cda9e68b9907ca637e
MeabhG97 <91346624+Mayv-dev@users.noreply.github.com>
2024-10-22 21:21:10 +0100

    Get Dog Fact

---
+++ Services/DogFactService.cs
@@ -0,0 +1,19 @@
+using RestSharp;
+using System.Text.Json;
+using Dogs.Types;
+
+namespace Dogs.Services;
+public class DogFactService
+{
+    static readonly string DOG_FACT_URL = "https://dogapi.dog/api/v2/facts";
+    public string GetFact()
+    {
+        var client = new RestClient(DOG_FACT_URL);
+        var req = new RestRequest();
+
+        var res = client.Execute(req);
+
+        var content = JsonSerializer.Deserialize<DogFactResponseWrapper>(res.Content);
+        return content.Data[0].Attirbutes.Body;
+    }
+}
---
+++ Types/DogFactResponse.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace Dogs.Types;
+public class DogFactResponse {
+    [JsonPropertyName("id")]
+    public required string Id {get; set;}
+
+    [JsonPropertyName("type")]
+    public required string Type {get; set;}
+
+    [JsonPropertyName("attributes")]
+    public required DogFact Attirbutes {get; set;}
+}
